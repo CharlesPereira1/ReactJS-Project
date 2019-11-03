9df0dbe41497972619eb3061899fe0d7eef4f455
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-02 22:52:18 -0300

    Carregando dados da API, Repository{} e Issues []

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {}, // unico repositorio inicia como objeto
+    issues: [], // como são varios issues então neste caso é array,
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    // api.githu.com/repos/rocketseat/unform
+    // api.githu.com/repos/rocketseat/unform
+
+    // const response = await api.get(`/repos/${repoName}`);
+    // const issues = await api.get(`/repos/${repoName}/issues`);
+
+    // trabalhar as duas uma seguida da outra
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return <h1>Repository</h1>;
+  }
 }
